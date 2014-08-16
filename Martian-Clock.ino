92e30288631b1a33fa42aee4df2a895155cd12c2
Nick Sayer <nsayer@users.noreply.github.com>
2014-08-16 14:22:19 -0700

    Update Martian-Clock.ino
    Add the option of using a 10 based crystal instead of a 2 based (that is 4.000 MHz instead of 4.096 Mhz).

--- Martian-Clock.ino
+++ Martian-Clock.ino
@@ -39,6 +39,19 @@
 // Turn this on to use the unused output as a debug output
 // #define DEBUG
 
+// Our design choices may wind up with a system clock of either 500 kHz
+// or 512 kHz. If it's 500 kHz, then we have to do some juggling to wind up
+// with the proper IRQS_PER_SECOND value of 10. To set that up, uncomment
+// this:
+// #define TEN_BASED_CLOCK
+
+#ifdef TEN_BASED_CLOCK
+#define CLOCK_CYCLES (64)
+// Don't forget to decrement the OCR0A value - it's 0 based and inclusive
+#define CLOCK_BASIC_CYCLE (48 - 1)
+#define CLOCK_NUM_LONG_CYCLES (53)
+#endif
+
 // We're going to set up a timer interrupt for every 100 msec, so what's 1/.1?
 // Note that while we're actually doing stuff, we *must* insure that we never
 // work through an interrupt. This is because we're not *counting* these
@@ -56,15 +69,14 @@
 // This will alternate the ticks
 #define TICK_PIN (lastTick == P0?P1:P0)
 
-unsigned char lastTick;
+static unsigned char lastTick;
 
 // We need to add 99 extra sleeps every 6 minutes (3600 systicks).
 // We do this evenly by doing 36 of them every 37 systicks, then
 // 63 of them every 36 systicks. 36+63 = 99 and 36*37+36*63 = 3600. QED.
-#define CYCLE_COUNT (99)
-#define SHORT_CYCLE (36)
-#define LONG_CYCLE (37)
-#define NUM_LONG_CYCLES (36)
+#define SOL_CYCLE_COUNT (99)
+#define SOL_CYCLE_LENGTH (36)
+#define NUM_LONG_SOL_CYCLES (36)
 
 // This delay loop is magical because we know the timer is ticking at 500 Hz.
 // So we just wait until it counts N/2 times and that will be an N msec delay.
@@ -73,13 +85,27 @@ static void delay_ms(unsigned char msec) {
    while(TCNT0 - start_time < msec / 2) ; // sit-n-spin
 }
 
+static void doSleep() {
+#ifdef TEN_BASED_CLOCK
+  static unsigned char cycle_pos = 0;
+
+  if (cycle_pos == CLOCK_NUM_LONG_CYCLES)
+    OCR0A = CLOCK_BASIC_CYCLE;
+  if (cycle_pos++ >= CLOCK_CYCLES) {
+    OCR0A = CLOCK_BASIC_CYCLE + 1;
+    cycle_pos = 0;
+  }
+#endif
+  sleep_mode();
+}
+
 // Each call to doTick() will "eat" a single one of our interrupt "ticks"
 static void doTick() {
   digitalWrite(TICK_PIN, HIGH);
   delay_ms(TICK_LENGTH);
   digitalWrite(TICK_PIN, LOW);
   lastTick = TICK_PIN;
-  sleep_mode(); // eat the rest of this tick
+  doSleep(); // eat the rest of this tick
 }
 
 ISR(TIMER0_COMPA_vect) {
@@ -87,16 +113,18 @@ ISR(TIMER0_COMPA_vect) {
 }
 
 void setup() {
+  // change this so that we wind up with a 512 kHz or a 500 kHz CPU clock.
+  // And if it's 500 kHz, be sure to uncomment TEN_BASED_CLOCK above.
   clock_prescale_set(clock_div_8);
   power_adc_disable();
   power_usi_disable();
   power_timer1_disable();
   TCCR0A = _BV(WGM01); // mode 2 - CTC
   TCCR0B = _BV(CS02) | _BV(CS00); // prescale = 1024
-  // xtal freq = 4.096 MHz.
-  // CPU freq = 4.096 MHz / 8 = 512 kHz
-  // count freq = 512 kHz / 1024 = 500 Hz
-  OCR0A = 49; // 10 Hz - don't forget to subtract 1 - the counter is 0-49.
+#ifndef TEN_BASED_CLOCK
+  // count freq = 512 kHz / 1024 = 500 Hz, and it never changes.
+  OCR0A = 49; // 10 Hz - don't forget to subtract 1 - the counter will count 0-49.
+#endif
   TIMSK = _BV(OCIE0A); // OCR0A interrupt only.
   ACSR = _BV(ACD); // Turn off analog comparator - but was it ever on anyway?
   
@@ -124,12 +152,13 @@ void loop() {
       if (i == 0)
         doTick();
       else
-        sleep_mode();
-      unsigned char this_bump_length = (bump_counter < NUM_LONG_CYCLES)?LONG_CYCLE:SHORT_CYCLE;
+        doSleep();
+      unsigned char this_bump_length = SOL_CYCLE_LENGTH;
+      if (bump_counter < NUM_LONG_SOL_CYCLES) this_bump_length++;
       if (sol_counter++ >= this_bump_length) {
-        sleep_mode(); // the extra sleep
+        doSleep(); // the extra sleep
         sol_counter = 0;
-        if (bump_counter++ >= CYCLE_COUNT) {
+        if (bump_counter++ >= SOL_CYCLE_COUNT) {
           bump_counter = 0;
         }
       }
