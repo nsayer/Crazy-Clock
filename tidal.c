46e1574c59119742c1b8c29eebef4acf6ebfe8ff
Nick Sayer <nsayer@arachne.dhcp>
2014-10-08 14:08:07 -0700

    Add the new tidal clock - 50:28 slow per day.

--- tidal.c
+++ tidal.c
@@ -1,6 +1,6 @@
 /*
 
- Martian Clock for Arduino
+ Tide Clock for Arduino
  Copyright 2014 Nicholas W. Sayer
  
  This program is free software; you can redistribute it and/or modify
@@ -20,14 +20,14 @@
 
 #include "base.h"
 
-// We need to add 39 minutes 36 seconds every day. That's means the
-// fraction is 36 + 12/33.
+// We need to add 50 minutes 28 seconds every day. That's means the
+// fraction is 28 + 404/757.
 // This is the whole number
-#define BASE_CYCLE_LENGTH (36)
+#define BASE_CYCLE_LENGTH (28)
 // This is the fractional numerator
-#define NUM_LONG_CYCLES (12)
+#define NUM_LONG_CYCLES (404)
 // This is the fractional denominator
-#define CYCLE_COUNT (33)
+#define CYCLE_COUNT (757)
 // To make the clock run a fraction slower rather than faster, uncomment this.
 #define RUN_SLOW
 
